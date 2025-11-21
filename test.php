04f52281118ca410ffd7afc938143f582e8dc2c0
enzo igual <igual@lan.sio.lyceefulbert.fr>
2025-11-21 13:30:01 +0100

    mouvement entre les couloirs

--- test.php
+++ test.php
@@ -19,52 +19,46 @@
     
 
     //stocker position de départ, $_GET pour pouvoir gérer la position inter pages.
-    if(isset($_GET['position'])==false)
+    if(isset($_GET['position']))
     {
-        $position=$depart
+        $position=$_GET['position'];
+    }
+    else
+    {  
+        $position=$depart;
+        
     }
 
-    // /!\ boucle while du jeu à décommenter quand nécessaire
-    $tour=0;
-    $demande=true;
-    while($tour<5){
     
-        // trouver les possibilités de déplacement à partir de la position actuelle
-        $requeteposs = 'SELECT couloir1,couloir2 FROM passage WHERE couloir1 = :position OR couloir2 = :position';
-        $prepareposs = $sqlite -> prepare($requeteposs);
-        $prepareposs -> bindValue(':position', $position, SQLITE3_INTEGER);
-        $resultposs = $prepareposs->execute();
-        $poss = array();
-        $nbcouloir = 0;
+    // trouver les possibilités de déplacement à partir de la position actuelle
+    $requeteposs = 'SELECT couloir1,couloir2 FROM passage WHERE (couloir1 = :position OR couloir2 = :position) AND passage.type="libre"';
+    $prepareposs = $sqlite -> prepare($requeteposs);
+    $prepareposs -> bindValue(':position', $position, SQLITE3_INTEGER);
+    $resultposs = $prepareposs->execute();
+    $poss = array();
+    $nbcouloir = 0;
 
-        //crée un array avec les positions possible à partir de la position actuelle
-        echo "<ul>";
-        while($result = $resultposs->fetchArray()){
-            $autre = ($result['couloir1'] == $position) ? $result['couloir2']:$result['couloir1'];
-            array_push($poss, $autre);
-            echo '<li><button onclick='."$position=Change_pos($poss[$nbcouloir])".'>'.$poss[$nbcouloir].'</button></li>';
-            $nbcouloir+=1;
-        }
-        echo "</ul>";
-
-        //Création de la page html avec les résultats qu'on a get depuis le début du code
-        echo "<!DOCTYPE html>\n";		//On demande un saut de ligne avec \n, seulement avec " et pas '
-            echo "<html lang=\"fr\"><head><meta charset=\"UTF-8\">\n";	//Avec " on est obligé d'échapper les " a afficher avec \
-            echo "<title>Liste des couloirs</title>\n";
-        echo "</head>\n";
-        echo "<body>\n";
-                echo "<h1>Liste des couloirs</h1>\n";
-                echo "<ul>";
-                echo "$depart, $fin";
-        echo "</ul>";
-        echo "</body>\n";
-        echo "</html>\n";
-
-        $tour=$tour+1;
+    //crée un array avec les positions possible à partir de la position actuelle
+    echo "<ul>";
+    while($result = $resultposs->fetchArray()){
+        $autre = ($result['couloir1'] == $position) ? $result['couloir2']:$result['couloir1'];
+        array_push($poss, $autre);
+        echo "<li><a href='test.php?position=$poss[$nbcouloir]'>$poss[$nbcouloir]</a></li>";
+        $nbcouloir+=1;
     }
+    echo "</ul>";
 
-    function Change_pos($new_pos)
-    {
-        return $new_pos;
-    }
+    //Création de la page html avec les résultats qu'on a get depuis le début du code
+    echo "<!DOCTYPE html>\n";		//On demande un saut de ligne avec \n, seulement avec " et pas '
+        echo "<html lang=\"fr\"><head><meta charset=\"UTF-8\">\n";	//Avec " on est obligé d'échapper les " a afficher avec \
+        echo "<title>Liste des couloirs</title>\n";
+    echo "</head>\n";
+    echo "<body>\n";
+            echo "<h1>Départ/Arrivée</h1>\n";
+            echo "<ul>";
+            echo "$depart, $fin";
+    echo "</ul>";
+    echo "</body>\n";
+    echo "</html>\n";
+
 ?>
