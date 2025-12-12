1d0a6e2ef60d410e907fff3378d26220fc530e10
enzo igual <igual@lan.sio.lyceefulbert.fr>
2025-12-12 16:54:37 +0100

    bug css grid

--- fonctions/Déplacement.php
+++ fonctions/Déplacement.php
@@ -3,17 +3,35 @@
     //Affiche les possibilités de déplacement.
     for($poss=0;$poss<count($possibilités);$poss++)
     {
+        echo "<div class = 'container'>";
+        echo "<item1></div>";
+        echo "<item3></div>";
+        echo "<item7></div>";
+        echo "<item9></div>";
         if(!in_array($position,$grille) || !in_array($possibilités[$poss],$grille))
         {
-                echo "<div class 'grid'>";
-                    echo "<div class 'cell'><a href='index.php?position=$possibilités[$poss]&key=$key'>$possibilités[$poss] $directionarr[$poss]</a></div>";
-                echo "</div>";
+            switch($directionarr[$poss]){
+                case 'N':
+                    echo "<itemN><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+                case 'O':
+                    echo "<itemO><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+                case 'E':
+                    echo "<itemE><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+                case 'S':
+                    echo "<itemS><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+                    
+            }
         }
         
         if(in_array($position,$grille) && in_array($possibilités[$poss],$grille) && ($_GET['key'] == 1 || $_SESSION['gripen'][$_SESSION['positgrille']] == 1))
         {
-            echo "<div class 'cell'><a href='index.php?position=$possibilités[$poss]&key=$key'>$possibilités[$poss] $directionarr[$poss]</a></div>";
+            echo "<div><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
         }
+        echo "</div>";
     }
     
 ?>
