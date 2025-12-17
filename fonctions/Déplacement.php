40d622b92817f7f7779bcb209e427c375fe8ab59
enzo <enzo@enzo>
2025-12-17 22:03:55 +0100

    continue front end

--- fonctions/Déplacement.php
+++ fonctions/Déplacement.php
@@ -7,7 +7,6 @@
         echo "<item1></div>";
         echo "<item3></div>";
         echo "<item7></div>";
-        echo "<item9></div>";
         if(!in_array($position,$grille) || !in_array($possibilités[$poss],$grille))
         {
             switch($directionarr[$poss]){
@@ -29,8 +28,22 @@
         
         if(in_array($position,$grille) && in_array($possibilités[$poss],$grille) && ($_GET['key'] == 1 || $_SESSION['gripen'][$_SESSION['positgrille']] == 1))
         {
-            echo "<div><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+            switch($directionarr[$poss]){
+                case 'N':
+                    echo "<itemN><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+                case 'O':
+                    echo "<itemO><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+                case 'E':
+                    echo "<itemE><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+                case 'S':
+                    echo "<itemS><a href='index.php?position=$possibilités[$poss]&key=$key'>$directionarr[$poss]</a></div>";
+                    break;
+            }
         }
+        echo "<item9></div>";
         echo "</div>";
     }
     
